5196fdc8b8bd97de37bf92869849364bf5503895
James Quintero <jquinte2@mail.sfsu.edu>
2016-09-01 16:00:29 -0700

    Removed personal info from about section

--- Journal/Form1.Designer.cs
+++ Journal/Form1.Designer.cs
@@ -484,18 +484,7 @@
             this.label1.Size = new System.Drawing.Size(192, 17);
             this.label1.TabIndex = 1;
             this.label1.Text = "Created by James Quintero";
-            // 
-            // label2
-            // 
-            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Maven Pro", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(422, 314);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(174, 17);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "jamesaquint@gmail.com";
-            this.label2.Visible = false;
+            
             // 
             // label5
             // 
